fa3e49bbbc72b290a1eae087fbd7766bb2278d77
Antimatter15 <Antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2007-09-02 19:36:15 +0000

    this is a random test rethis is a random test revision.
    and right now, i'm debating if i should kill my computer by uploading the new beta test version
    
    okay, so this time I's FOR REAL!
    
    ..added gzip.php, a component in the new version that i accidentally placed here

---
+++ Animator/php/gzip.php
@@ -0,0 +1,7 @@
+<?php 
+ob_start ("ob_gzhandler");
+include "jsmin.php";
+header("Content-type: text/javascript; charset: UTF-8");
+$fh = fopen($_REQUEST['url'], 'r');
+echo JSMin:minify(fread($fh, filesize($_REQUEST['url'])));
+?>
