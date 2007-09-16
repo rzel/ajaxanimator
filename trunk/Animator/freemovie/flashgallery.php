9ccbeae0328db04d939bcfed9cdf770235eae67e
Antimatter15 <Antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2007-09-16 15:24:48 +0000

    re-fixed compiler.php.exe changed ajaxanimator.htm's save as text display error

--- trunk/Animator/freemovie/flashgallery.php
+++ trunk/Animator/freemovie/flashgallery.php
@@ -9,14 +9,28 @@ All of these animations were made and published by the Ajax Animator-A fully web
 <?php
 if ($handle = opendir('files')) {
     while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
+        if ($file != "." && $file != ".." && $file != "index.php") {
 echo "<object width='240' height='136'><param name='movie' value='files/$file'>";
 echo "<embed src='files/$file' width='240' height='136'></embed></object>";
         }
     }
     closedir($handle);
 }
-?> 
+?>
+<hr>
+These next animations are previewed, but not exported.
+<hr>
+ <?php
+if ($handle = opendir('preview')) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != ".." && $file != "index.php") {
+echo "<object width='240' height='136'><param name='movie' value='preview/$file'>";
+echo "<embed src='preview/$file' width='240' height='136'></embed></object>";
+        }
+    }
+    closedir($handle);
+}
+?>
 </center>
 </body>
 </html>
