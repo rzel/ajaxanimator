62533a5a7c94a900f48dab2236ab3d16d9def7f3
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-04-21 01:07:56 +0000

    AjaxAnimator 0.2 Development Build

---
+++ trunk/Animator2/js/ajaxanimator.js
@@ -0,0 +1,28 @@
+/**
+ * Ajax Animator
+ *
+ * @author    Antimatter15
+ * @copyright (c) 2007-2008, by Antimatter15
+ * @date      14. April 2008
+ * @version   0.20+
+ *
+ * @license application.js is licensed under the terms of the Open Source GPL 2.0 license. 
+ * 
+ * License details: http://www.gnu.org/licenses/gpl.html
+ */
+ 
+/*global Ext, Application */
+ 
+Ext.BLANK_IMAGE_URL = '../theme/images/default/s.gif';
+Ext.ns('ajaxanimator');
+ 
+// application main entry point
+Ext.onReady(function() {
+ 
+    Ext.QuickTips.init();
+ 
+    // code here
+ 
+}); // eo function onReady
+ 
+// eof
