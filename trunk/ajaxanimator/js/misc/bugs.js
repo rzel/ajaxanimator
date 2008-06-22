8342bf06df9d2376ccef8580974a8279c60a1461
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-06-22 01:18:51 +0000

    changed error messages a bit. (actually, more of removed them) Renamed function from Ax.error to Ax.bugreport

--- trunk/ajaxanimator/js/misc/bugs.js
+++ trunk/ajaxanimator/js/misc/bugs.js
@@ -1,11 +1,11 @@
-Ax.Error = function(){
+Ax.BugReport = function(){
 Ax.gs(4);
 var ErrorWindow = new Ext.Window({
     closable: true,
     width: 410,
     height: 300,
     minimizable: true,
-	title: "Ajax Animator has encountered a problem",
+	  title: "Report Bug",
     border: false,
     plain: true,
     layout: 'border',
@@ -19,7 +19,7 @@ var ErrorWindow = new Ext.Window({
     },{
 		text: 'Send (recomended)',
 		handler: function(){
-			Ax.msg("Sending Bug Report","We are sending your bug report");
+			Ax.msg("Sending Bug Report","We are sending your bug report... Or at least we'll make you think we are.");
 			ErrorWindow.close();
 		}
 	}
@@ -29,11 +29,7 @@ var ErrorWindow = new Ext.Window({
 	split: true,
 	height: 80,
 	region: "north",
-	html: "<b>Ajax Animator has encountered a problem."+
-	" This is likely due to a bug in the software. You may"+
-	" continue using the software as normal, but there might"+
-	" be some issues. We are sorry for the inconvienience, "+
-	"and you may submit a bug report for us to fix it.</b><br />"
+	html: "Hai"
 	},{
 	region: "center",
 	html: "scarey"
