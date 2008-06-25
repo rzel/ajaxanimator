329a15dff9dc2a0d6e453290caf997bf6411766e
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-06-25 00:16:30 +0000

    combined var statements

--- ajaxanimator/js/animation/tween.js
+++ ajaxanimator/js/animation/tween.js
@@ -10,11 +10,16 @@ Ax.tween = function(frame1,frame2,layer){
 }
 
 Ax.getSFTween = function (frame, frame1, frame2,layer){
+  if(!frame){frame = Ax.tcurrent.frame}
+  if(!layer){layer = Ax.tcurrent.layer}
+  if(!frame1){Ax.largest_nonempty(frame,layer)}
+  if(!frame2){Ax.smallest_nonempty(frame,layer)}
+  
   //console.log(frame, frame1,frame2,layer)
   //tween a single frame and get a single frame (framedump) in return
-  var frame1_dump = Ax.canvas_storage[layer][frame1];//load dumps
-  var frame2_dump = Ax.canvas_storage[layer][frame2];//load dumps
-  var tween_frame = Ext.ux.clone(frame2_dump); //clone frame2
+  var frame1_dump = Ax.canvas_storage[layer][frame1], //load dumps
+      frame2_dump = Ax.canvas_storage[layer][frame2], //load dumps
+      tween_frame = Ext.ux.clone(frame2_dump); //clone frame2
         
   for(var o = 0; o < frame1_dump.length; o++){//loop through shapes
     if(frame2_dump[o]){ //continue only if the second frame has it too
@@ -28,6 +33,7 @@ Ax.getSFTween = function (frame, frame1, frame2,layer){
   }
   return tween_frame;
 }
+
 
 Ax.tweenNumber = function(frame1, frame2, value1, value2, index){//frame1, frame2, first number, second number, index (from first)
   //no type checking yet, cause i dont feel like it
