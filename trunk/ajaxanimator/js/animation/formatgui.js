4ee9e8cfbdab65bab8769d79cc7bc34acfd76758
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-06-29 19:56:09 +0000

    read the magickal contents of this file. (I use magickal too much don't I?)

---
+++ trunk/ajaxanimator/js/animation/formatgui.js
@@ -0,0 +1,11 @@
+/*
+* all the remarkably interesting code that displays that box to show you the markup
+* that magical dialog that asks if it wants to steal all your private files on your drive
+* that cool box requesting to steal items on your clipboard
+* that field that attempts to steal your favorite sites
+* that stuff that goes on that tries to carpetbomb your desktop with random aliens :P
+* yep, all that magic takes place..... here.
+*
+* actually, all of that's a lie. Nothing's in here. 
+*/
+
