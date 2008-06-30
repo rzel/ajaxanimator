509925d8385d4549788af001ba2efb032548a740
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-06-30 23:12:52 +0000

    Ax.utils are no longer necessary.

--- trunk/ajaxanimator/js/misc/misc.js
+++ trunk/ajaxanimator/js/misc/misc.js
@@ -3,18 +3,6 @@ A whole lot of random scripts
 */
 
 
-Ax.util = {
-htmlentities : function(s){
-//Slightly compressed version of the htmlentities function combined with nl2br
-//  original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net) from php.js
-
-var div = document.createElement('div'), text = document.createTextNode(s);
-div.appendChild(text);
-return div.innerHTML.replace(/([^>])\n/g, '$1<br />\n')
-}
-
-}
-
 
 
 Ax.macroExec = function(){
