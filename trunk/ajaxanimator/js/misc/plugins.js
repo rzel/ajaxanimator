cfc6b46039ce5482ed295a5a4b61182ee8f1ba04
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-06 16:08:03 +0000

    beginnings of a random shape plugin

--- trunk/ajaxanimator/js/misc/plugins.js
+++ trunk/ajaxanimator/js/misc/plugins.js
@@ -5,29 +5,41 @@
 
 Ax.plugins = {}
 
-Ax.plugins["wave"] = function(mx,my){
+Ax.plugins["Wave"] = function(mx,my){
   Ax.api.loop.frame(function(shape){
     return Ax.api.transform.translate(shape,0,mx*Math.sin(shape.left/shape.width*my))
   })
 }
 
-Ax.plugins["brick"] = function(height, width){
+Ax.plugins["Brick Wall"] = function(sx, sy, height, width, brickheight, brickwidth){
   for(var x = 0; x < width; x++){
     for(var y = 0; y < height; y++){
       Ax.api.create.shape({
         type: "rect",
-        top: 120+(y*10),
-        left: 50+(x*10),
-        width: 10,
-        height: 10
+        top: sy+(y*brickheight),
+        left: sx+(x*brickwidth),
+        width: brickwidth,
+        height: brickheight
         });
     }
   }
 }
 
-Ax.plugins["explode"] = function(){
+
+
+Ax.plugins["Explode"] = function(){
   Ax.api.loop.frame(function(shape){
     return Ax.api.transform.absolute(shape, Math.round(Math.random()*Ax.canvasHeight), Math.round(Math.random()*Ax.canvasWidth));  
   })
+}
+
+Ax.plugins["Random Shape"] = function(){
+  Ax.api.create.shape({
+    type: "rect",
+    top: Math.random()*Ax.canvasHeight,
+    left: Math.random()*Ax.canvasWidth,
+    width: Math.random()*Ax.canvasHeight,
+    height: Math.random()*Ax.canvasHeight
+  });
 }
 
