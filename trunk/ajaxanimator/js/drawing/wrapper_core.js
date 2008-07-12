d370a9ea62d10fcad7f76fa398de6aea010963eb
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-12 22:01:17 +0000

    renamed wrapper stuffs, and new save pr0xy

---
+++ trunk/ajaxanimator/js/drawing/wrapper_core.js
@@ -0,0 +1,70 @@
+/**
+ * @author antimatter15
+ */
+
+Ax.loadShapes = function(shapes, noattachlistener, instance){ //instance; probably need a better name for this. but its the richdraw/onlypaths/viewer/etc reference. 
+    if (typeof shapes == typeof "aimis5wnaatte1tromese") { //try un-scrabling this!
+        shapes = Ext.util.JSON.decode(shapes);
+    }
+    for (var i = 0; i < shapes.length; i++) {
+        Ax.loadShape(shapes[i], noattachlistener, instance);
+    }
+}
+
+Ax.loadShape = function(shape, noattachlistener, instance){
+	  var newshape  = ((instance)?instance:Ax.canvas).renderer.create(shape.type, //Shape
+	  (shape.fillColor)?shape.fillColor:("#"+Ax.Color.fill), 
+	  (shape.lineColor)?shape.lineColor:("#"+Ax.Color.line), 
+	  (shape.fillOpac)?shape.fillOpac:1,
+	  (shape.lineOpac)?shape.lineOpac:1, 
+	  (shape.lineWidth)?shape.lineWidth:Ax.Color.width, 
+	  (shape.left)?shape.left:100,
+	  (shape.top)?shape.top:100, 
+	  (shape.width)?shape.width:100,
+	  (shape.height)?shape.height:100,
+	  (shape.text)?shape.text:'',
+	  (shape.textSize)?shape.textSize:19,
+	  (shape.textFamily)?shape.textFamily:'Arial',
+	  (shape.href)?shape.href:'', 
+	  (shape.points)?shape.points:'', 
+	  (shape.transform)?shape.transform:'',
+	  (shape.parent)?shape.parent:''); 
+ 
+    newshape.id = (shape.id) ? shape.id : createUUID()
+    
+    if (!noattachlistener) {
+        Ext.get(newshape).on("mousedown", Ax.canvas.onHit, Ax.canvas);
+    }
+}
+
+Ax.dumpshape = function(shape){
+    if (!shape.getAttribute("transform")) {
+		try {
+        	var box = shape.getBBox()
+        	shape.setAttributeNS(null, 'transform', 'rotate(0, ' + (box.x + (box.width / 2)) + ', ' + (box.y + (box.height / 2)) + ')');
+    	}catch(err){
+			//OMG!
+		}
+	}
+    return Ax.canvas.info(shape)
+}
+
+Ax.dumpshapes_core = function(){
+    var rawshapes = Ax.canvas.getshapes()
+    var newshapes = [];
+    for (var i = 0; i < rawshapes.length; i++) {
+        if (rawshapes[i].id != "tracker") {
+            newshapes.push(Ax.dumpshape(rawshapes[i]));
+        }
+    }
+	return newshapes
+}
+
+Ax.dumpshapes = function(format){
+    if (format == "json") {
+        return Ext.util.JSON.encode(Ax.dumpshapes_core())
+    }
+    else {
+        return Ax.dumpshapes_core();
+    }
+}
--- trunk/ajaxanimator/server/user/save_proxy.php
+++ trunk/ajaxanimator/server/user/save_proxy.php
@@ -1,14 +1,15 @@
 <?php
-/**
- * @author antimatter15
- */
-if($_REQUEST["action"] == "test"){
-	die("working"); //hmm... I don't want to die working... Especially for this project....
-}elseif($_REQUEST["action"] == "save"){
-	//you know how I said I wouldn't reuse code from the old one? well this is sorta an exception.
-	header('Content-type: application/octetstream');
-	header('Content-Length: ' . strlen($_REQUEST['data']));
-	header('Content-Disposition: attachment; filename="'.$_REQUEST['name'].'"');
-	echo stripslashes($_REQUEST['data']);
-}
+	/**
+	 * @author antimatter15
+	 * this is a file thingy. it basically pulls data, and spits it out again with new headers
+	 */
+	if($_REQUEST["action"] == "test"){
+		die("working"); //hmm... I don't want to die working... Especially for this project....
+	}elseif($_REQUEST["action"] == "save"){
+		//you know how I said I wouldn't reuse code from the old one? well this is sorta an exception.
+		header('Content-type: application/octetstream');
+		header('Content-Length: ' . strlen($_REQUEST['data']));
+		header('Content-Disposition: attachment; filename="'.$_REQUEST['name'].'"');
+		echo stripslashes($_REQUEST['data']);
+	}
 ?>
