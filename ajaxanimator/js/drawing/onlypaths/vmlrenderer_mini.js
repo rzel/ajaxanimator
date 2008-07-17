98e81a865afc84869ef4702460dc3784b2d91da9
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-17 20:34:19 +0000

    vmlrenderer_mini.js supports IE!!!!!!!!!!!!!!!!!! Standalone animation player works in IE too!

--- ajaxanimator/js/drawing/onlypaths/vmlrenderer_mini.js
+++ ajaxanimator/js/drawing/onlypaths/vmlrenderer_mini.js
@@ -1,4 +1,3 @@
-
 function VMLRenderer() {
 	this.base = AbstractRenderer;
 }
@@ -16,15 +15,13 @@ VMLRenderer.prototype.init = function(elem)
   elem.ownerDocument.namespaces.add("v", "urn:schemas-microsoft-com:vml");
   var style = elem.ownerDocument.createStyleSheet();
   style.addRule('v\\:*', "behavior: url(#default#VML);");
+  //style.coordsize=screen.availWidth+","+screen.availHeight;
  }
 
-VMLRenderer.prototype.removeAll = function(){
-	while (this.container.hasChildNodes()) {
-		this.container.removeChild(this.container.firstChild);
-	}
-}
 
-VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, lineOpac, lineWidth, left, top, width, height) {
+
+
+VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, lineOpac, lineWidth, left, top, width, height, textMessaje, textSize, textFamily, imageHref, points, transform, parent) {
   var vml;
     var shap=1;
      if (shape == 'rect') {
@@ -35,7 +32,7 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     vml.style.top = top;
     vml.style.width = width;
     vml.style.height = height;
-    if (fillColor != '') {
+    if (fillColor != ''  || fillColor != 'none') {
       vml.setAttribute('filled', 'true');
       vml.setAttribute('fillcolor', fillColor); 
       
@@ -53,7 +50,7 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
             vml.style.height = height;
             vml.setAttribute('arcsize', '20%');
             if(fillColor != '') {
-              vml.setAttribute('filled', 'true');
+              vml.setAttribute('filled', 'true'  || fillColor != 'none');
               vml.setAttribute('fillcolor', fillColor);
             }
             else {
@@ -67,7 +64,7 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     vml.style.width = width; 
     vml.style.height = height;
     vml.style.position = 'absolute';
-              if (fillColor != '') {
+              if (fillColor != ''  || fillColor != 'none') {
               vml.setAttribute('filled', 'true');
               vml.setAttribute('fillcolor', fillColor);
             }
@@ -93,14 +90,14 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
          thispath+=' '+xpArray[conta]+', '+ypArray[conta];
      } 
     //vml.setAttribute("coordsize","800,600");
-    vml.setAttribute("points", thispath); 
+    vml.setAttribute("points", points); 
     //vml.setAttribute("coordorigin","0 0");   
     //vml.setAttribute("coordsize","200 200");
     //vml.setAttribute("path", "m 8,65 l 72,65, 92,11, 112,65, 174,65, 122,100, 142,155, 92,121, 42,155, 60,100 x e;
     //vml.setAttribute("path", "M320 32 c"+ thispath+" z ");     
     //vml.setAttribute("v", "m 10,70 l 85,10,160,70,160,160,10,160 x e");
     if (fillColor != '') {
-      vml.setAttribute('filled', 'true');
+      vml.setAttribute('filled', 'true'  || fillColor != 'none');
       vml.setAttribute('fillcolor', fillColor);
     }
     else {
@@ -128,7 +125,8 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     //vml.setAttribute("coordsize","200 200");
     //vml.setAttribute("path", "m 8,65 l 72,65, 92,11, 112,65, 174,65, 122,100, 142,155, 92,121, 42,155, 60,100 x e;
     var path01 = this.container.ownerDocument.createElement('v:path');  
-    path01.setAttribute("v", "m "+thispath+" c"+ thispath2+" e ");  
+    path01.setAttribute("v", points);
+    //path01.setAttribute("v", "m "+thispath+" c"+ thispath2+" e ");  
     vml.style.position="absolute";  
     vml.style.width= 700+"px";
     vml.style.height=500+"px";
@@ -148,7 +146,7 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     //vml.setAttribute("coordsize", size);
     //vml.setAttribute("textpathok", "true");
     //vml.appendChild('');
-    if (fillColor != '') {
+    if (fillColor != ''  || fillColor != 'none') {
       vml.setAttribute('filled', 'true');
       vml.setAttribute('fillcolor', fillColor);
     }
@@ -174,7 +172,7 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     var path01 = this.container.ownerDocument.createElement('v:path');  
     path01.setAttribute('v', 'm '+left+','+top+' c'+(left+1)+','+(top+1)+' e ');  
 
-     if (fillColor != '') {
+     if (fillColor != ''  || fillColor != 'none') {
       vml.setAttribute('filled', 'true');
       vml.setAttribute('fillcolor', fillColor);
      }
@@ -185,20 +183,18 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
     
     vml.appendChild(path01)
     } 
-
-
     else if (shape == 'image') {   
-    var data =document.forms[0].option_text_message.value;
+    var data =imageHref;//document.forms[0].option_text_message.value;
     vml = this.container.ownerDocument.createElement('v:image'); 
-    vml.setAttribute('src',data_image_href);
+    vml.setAttribute('src',imageHref);
     vml.style.position="absolute";  
-    vml.style.width=100+"px";
-    vml.style.height=100+"px";
+    vml.style.width=width+"px";
+    vml.style.height=height+"px";
     vml.style.left=left+"px";  
     vml.style.top=top+"px";  
     vml.style.margin=0+"px";  
     vml.style.padding=0+"px";  
-    if (fillColor != '') {
+    if (fillColor != ''  || fillColor != 'none') {
       vml.setAttribute('filled', 'true');
       vml.setAttribute('fillcolor', fillColor);
     }
@@ -212,114 +208,169 @@ VMLRenderer.prototype.create = function(shape, fillColor, lineColor, fillOpac, l
    
     }
     
-    else if (shape == 'text') {   
+ else if (shape == 'textpath') 
+  {   
    
-    var data =document.forms[0].option_text_message.value;
+    var data =textMessaje;
     vml = this.container.ownerDocument.createElement('v:shape');
     vml.style.position="absolute";  
-    vml.style.width=100+"px";
-    vml.style.height=100+"px";
+    //vml.setAttribute('coordorigin',left+', '+top);
+     //vml.style.coorsize= '';
+     coordorigin="0, 0"
+    vml.style.width= 100+'px'
+    vml.style.height=100+'px'
     vml.style.left=left+"px";  
     vml.style.top=top+"px";  
-    vml.style.margin=0+"px";  
+    vml.style.margin=0+"px"; 
+    //vml.style.centerX=left+"px";  
+    //vml.setAttribute('center-x',(left+300)+'px');  
     vml.style.padding=0+"px";  
-    if (fillColor != '') {
+    if (fillColor != ''  || fillColor != 'none') 
+     {
       vml.setAttribute('filled', 'true');
       vml.setAttribute('fillcolor', fillColor);
-    }
-    else {
+     }
+      else
+     {
       vml.setAttribute('filled', 'false');
-    }
-    vml.setAttribute('strokeweight', parseFloat(lineWidth)+'px');//document.forms[0].linewidth.options[document.forms[0].linewidth.selectedIndex].value+'px');
-   vml.setAttribute('stroked', 'true');  
-   vml.setAttribute('strokecolor',lineColor);//document.forms[0].linecolor.options[document.forms[0].linecolor.selectedIndex].value);
-    //vml.setAttribute('fillcolor', fillColor);  
-    vml.setAttribute('path','m 100 100 l 600 100 e'); 
-        var textPathObj = this.container.ownerDocument.createElement("v:textpath");
-        //textPathObj.setAttribute('style','font:'+document.forms[0].option_text_family.options[document.forms[0].option_text_family.selectedIndex].value+' normal normal '+parseFloat(document.forms[0].option_text_size.value)+'px ');
-        textPathObj.setAttribute('string', data);  
-        textPathObj.setAttribute('trim', 'false'); 
-        textPathObj.setAttribute('fitpath', 'false');
-        textPathObj.setAttribute('on','true');  
-        
-        textPathObj.style.fontFamily=document.forms[0].option_text_family.value;  
-        textPathObj.style.fontSize=parseFloat(document.forms[0].option_text_size.value)+'px'; 
-         textPathObj.style.vTextKern='true';  
-      var pathObj = this.container.ownerDocument.createElement("v:path");
-      pathObj.setAttribute('textpathok', 'true');  
-        //textPathObj.setAttribute('style','font-family:"Arial Black";font-size:40pt;v-text-kern:t');
-
- 
-  /* //vml.setAttribute('coordsize', '700,500'); 
-    vml.setAttribute('inset', '0px, 0px, 0px, 0px');
-    if (fillColor != '') 
+     }
+    if(lineWidth==0 || lineWidth=='none' || lineColor== 'none' ) 
      {
-      vml.setAttribute('stroked', 'true');
-      vml.setAttribute('fillcolor', fillColor);
-     } 
-      else 
-     {
-      vml.setAttribute('stroked', 'false');
-     }    
-   
-    
-    var fontObj =this.container.ownerDocument.createElement("font");  
-      fontObj.face = document.forms[0].option_text_family.value;
-      fontObj.size = parseFloat(document.forms[0].option_text_size.value);   
-       fontObj.color = fillColor;  
-       fontObj.innerHTML = data;
-         
-      
- 
-        
- 
-     var fillObj = this.container.ownerDocument.createElement("v:fill");
-          fillObj.setAttribute('color', fillColor);
-          fillObj.setAttribute('on', 'true');
-      var lineObj = this.container.ownerDocument.createElement('v:line'); 
-    lineObj.style.position = 'absolute';
-    lineObj.setAttribute('from', left + 'px,' + top + 'px');
-    lineObj.setAttribute('to', (left+100) + 'px,' + (top+0) + 'px');
-       */ 
-        //textPathObj.style.
-       //textPathObj.appendChild(fontObj); 
-       
-       //fontObj.stroked='true'; 
-       
-       //strokeObj.strokeweight='7px';
-       // strokeObj.color = fillColor;   
-       //strokeObj.on='true';
+         vml.setAttribute('stroked', 'false');  
+     }
+      else
+     {   
+         vml.setAttribute('stroked', 'true');
+         vml.setAttribute('strokeweight', parseFloat(lineWidth)+'px');//document.forms[0].linewidth.options[document.forms[0].linewidth.selectedIndex].value+'px');
+         vml.setAttribute('strokecolor',lineColor);//document.forms[0].linecolor.options[document.forms[0].linecolor.selectedIndex].value);
+     }   
+     vml.setAttribute('path','m '+0+' '+0+' r '+(700)+' '+0+' e'); 
+     var textPathObj = this.container.ownerDocument.createElement("v:textpath");
+     textPathObj.setAttribute('string', data); 
+      textPathObj.setAttribute('fitshape','false'); 
+     textPathObj.setAttribute('trim', 'false'); 
+     textPathObj.setAttribute('fitpath', 'false');
+     textPathObj.setAttribute('on','true');  
+     textPathObj.style.fontFamily=textFamily;  
+     textPathObj.style.fontSize=parseFloat(textSize)+'px'; 
+     textPathObj.setAttribute('vTextKern','true');  
+     textPathObj.setAttribute('text-align','right');   
+     //? textPathObj.setAttribute('v-rotate-letters','true'); 
+     var pathObj = this.container.ownerDocument.createElement("v:path");
+     pathObj.setAttribute('textpathok', 'true');  
      
-       
-      //textBoxObj.appendChild(strokeObj);
- 
-    //vml.appendChild(lineObj); 
-    
-    vml.appendChild(textPathObj);  
-    vml.appendChild(pathObj);  
+     //var textHandles = this.container.ownerDocument.createElement("v:handles");  
+     //textHandles.setAttribute('position')='bottomRight';
+
+     //vml.appendChild(textHandles);  
+     vml.appendChild(textPathObj);  
+     vml.appendChild(pathObj);  
     
     
   }    
- 
-  
-  if (lineColor != '') {
-    vml.setAttribute('stroked', 'true');
-    vml.setAttribute('strokecolor', lineColor);
-    vml.setAttribute('strokeweight', lineWidth);
-    var fill = this.container.ownerDocument.createElement('v:fill'); 
-      fill.setAttribute("opacity", parseFloat(fillOpac));
-                 vml.appendChild(fill);
+
+
     
+ else if (shape == 'text') 
+  { 
+    //style="position: absolute; width: 500; height: 500; left:300; top:50;"   
+    //coordorigin="-30 -30" coordsize="500 500"  
+    vml = this.container.ownerDocument.createElement('v:shape');
+    vml.style.position="absolute";  
+    //vml.setAttribute('coordorigin',0+', '+0);
+   
+     vml.style.coordsize=1000+', '+1000;
+     //vml.style.coorsize= '';
+    // coordorigin="0, 0"
+    vml.style.width= 1000;
+    vml.style.height=1000;
+    vml.style.left=left;  
+    vml.style.top=top-parseFloat(textSize);  
+ 
+    //vml.style.centerX=left+"px";  
+    //vml.setAttribute('center-x',(left+300)+'px');  
+    vml.style.padding=0;  
+    var textBox = this.container.ownerDocument.createElement("v:textbox");
+
+
+
+     /*<v:textbox> <div style="font-family: Verdana;" ><b style="color:purple">C</b>
+            <b style="color:rgb(153, 50, 204)">U</b>
+            <b style="color:rgb(255, 0, 255)">R</b>
+            <b style="color:red; font-size:20;">V</b>
+            <b style="color:rgb(255, 192, 203); font-size:20;">E</b>
+            </div>
+       */     
+     //textBox.textContent='<div style="font-family: Verdana;">'+textMessaje+'</div>';
+     var divtext = this.container.ownerDocument.createElement("div");  
+     divtext.style.coordorigin=0+', '+(0);      
+     divtext.style.margin=0;   
+     divtext.style.fontFamily=textFamily; 
+     divtext.style.margin=0; 
+     divtext.style.padding=0;
+     
+     
+     divtext.style.fontSize=parseFloat(textSize); 
+     divtext.style.color="#000000";  
+     divtext.innerHTML=textMessaje;
+     //textBox.textContent='<div style="font-family: Verdana; font-size:40; color:rgb(255, 0, 255)">'+textMessaje+'</div>';              
+     textBox.appendChild(divtext);       
+    vml.appendChild(textBox);  
+    
+  }    
+    else if (shape == 'group') {   
+    vml = this.container.ownerDocument.createElement('v:group'); 
+    vml.style.left=left+"px";  
+    vml.style.top=top+"px";  
   }
-  else {
-    vml.setAttribute('stroked', 'false');  
-    var stroke = this.container.ownerDocument.createElement('v:stroke'); 
-      stroke.setAttribute("opacity", parseFloat(lineOpac));
-                 vml.appendChild(stroke);
+ 
+  if(shape == 'zoom') 
+  {
+        
+  }else
+  {                                          
+       if(transform!='')
+        {
+         //svg.setAttributeNS(null, 'transform', transform);      
+        }
+  
+        if(shape != 'image' )
+         {  
+                if(lineWidth==0 || lineWidth=='none' || lineColor== 'none' )    
+                {
+                  vml.setAttribute('stroked', 'false');  
+                }
+                 else 
+                {
+                  vml.setAttribute('stroked', 'true');
+                  vml.setAttribute('strokecolor', lineColor);
+                  vml.setAttribute('strokeweight', lineWidth);
+                  var stroke = this.container.ownerDocument.createElement('v:stroke'); 
+                  stroke.setAttribute("opacity", parseFloat(lineOpac));
+                  vml.appendChild(stroke);
+                }
+
+               if (fillColor != ''  || fillColor != 'none') 
+                {
+                  var fill = this.container.ownerDocument.createElement('v:fill'); 
+                  fill.setAttribute("opacity", parseFloat(fillOpac));
+                  vml.appendChild(fill);
+                }
+          }     
+  } 
+  if(parent==''){
+   this.container.appendChild(vml);
+  }else{     
+   var parentShape = document.getElementById(parent);
+   parentShape.appendChild(vml);
   }
-  
-  
-  this.container.appendChild(vml);
   
   return vml;
 };  
+
+VMLRenderer.prototype.removeAll = function() {  
+ while( this.container.hasChildNodes () ){
+   this.container.removeChild( this.container.firstChild );
+   //this.container.removeNode( this.container.firstChild );
+ }
+};
--- ajaxanimator/js/drawing/view_wrapper.js
+++ ajaxanimator/js/drawing/view_wrapper.js
@@ -12,9 +12,34 @@ okay, this file has prettymuch been diversified from a simple preview.js stuff
 to a flexible implementation that probably should work standalone.
 */
 
+//a little hack to get IE to work
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 
 Ax.framerate = 12;
 //*
+
 
 Ax.init_view = function(element){
   element.innerHTML = "";
@@ -31,11 +56,14 @@ Ax.init_view_core = function(element){
   }
   return new RichDrawViewer(element, renderer);
 }
+
+
 Ax.viewer_load_frame = function(frame, layers, canvas, tweenfunc){
   //note: this function is not multi-layer friendly yet.
   //un-note: this function should be multi-layer friendly, but layers aren't even really supported so i donno
   canvas.renderer.removeAll();
   for(var layer in layers){
+  //alert([1, 2, 3, 4, 5].indexOf(2))
 		if(layers[layer].keyframes.indexOf(frame) != -1){
 			Ax.loadShapes(layers[layer].src[frame],  true, canvas);
 		}else{
