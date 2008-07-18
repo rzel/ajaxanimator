c455c9e8bc20290cf75cc2ad5e4bd7a7adda8b26
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-18 21:00:59 +0000

    huh? this is weird....

--- trunk/ajaxanimator/js/data/array.js
+++ trunk/ajaxanimator/js/data/array.js
@@ -1,35 +1,27 @@
 /**
  * @author antimatter15
- * Somewhat of an API for developing random format supports
+ * This is sorta critical part of the other formats.
+ * It makes making other formats easier
+ * Especially those incapable of porting the tweening engine to
  */
-
 Ax.export_array = function(){ //returns an array of every single frame (single layered) tweened when necessary
-
-
-var layers = Ax.export_animation_core(), output = [];
-for(var layer in layers){
-	if (layers[layer].keyframes.indexOf(frame) != -1) {
-		
-	}else{
-		
-	}
+    var layers = Ax.export_animation_core(), output = [], frame = 1
+    for (;; frame++) {
+        output.push([]);
+        for (var layer in layers) {
+            if (layers[layer].keyframes.indexOf(frame) != -1) {
+                output[frame - 1] = output[frame - 1].concat(layers[layer].src[frame])
+            }
+            else {
+                output[frame - 1] = output[frame - 1].concat(Ax.getSFTween(frame, Ax.largest_nonempty(frame, layer, layers), Ax.smallest_nonempty(frame, layer, layers), layer, layers[layer].src))
+            }
+            if (layers[layer].keyframes.sort(function(a, b){
+                return b - a
+            })[0] ==
+            frame) {
+                return output;
+            }
+        }
+    }
+    //is it possible that an infinite loop finishes?
 }
-
-  for(var layer in layers){
-  //alert([1, 2, 3, 4, 5].indexOf(2))
-		if(layers[layer].keyframes.indexOf(frame) != -1){
-			Ax.loadShapes(layers[layer].src[frame],  true, canvas);
-		}else{
-		  Ax.loadShapes(((tweenfunc)?tweenfunc:Ax.getSFTween)(frame, 
-											Ax.largest_nonempty(frame,layer,layers), 
-											Ax.smallest_nonempty(frame,layer,layers),
-											layer,layers[layer].src),  true, canvas);
-		}
-    if(layers[layer].keyframes.sort(function(a,b){return b - a})[0] == frame){
-  		return 0;
-		}
-  }
-  return frame
-	
-}
- 
