97fbe18521d6d7b8137025767c78b23a9ce2ce90
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-18 21:23:47 +0000

    designed a bit for the processing thing.

--- ajaxanimator/js/data/processing.js
+++ ajaxanimator/js/data/processing.js
@@ -1,3 +1,32 @@
 /**
  * @author antimatter15
  */
+
+
+ /*
+  
+  int frame = 0;
+  int frame_total = 0;
+  
+  void setup()
+  {
+    size(height, width)
+    frameRate(framerate)
+  }
+  
+  void draw(){
+    if(frame < frame_total){
+      frame++;
+    }else{
+      frame=1;
+    }
+    
+    switch(frame){
+      case 1:
+        frame1();
+        break;
+    }
+  }
+  
+  
+  */
