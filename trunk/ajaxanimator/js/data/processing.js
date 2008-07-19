ba34656c3a53495f4574c3233c45c7bbfa342067
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-19 01:20:38 +0000

    Processing almost-worksish!
    Ax.msg("",Ax.formats.processing().replace(/\n/g,"<br>"))
    
    added file for swfc.

--- trunk/ajaxanimator/js/data/processing.js
+++ trunk/ajaxanimator/js/data/processing.js
@@ -30,3 +30,51 @@
   
   
   */
+ 
+ Ax.formats.processing = function(){
+ 	var data = Ax.formats.array(), //the magic behind it all;
+				setup = ["void setup(){", //the function start
+				"size(480, 272)", //set the size of the canvas
+				"frameRate(12)", //set the framerate (fps)
+				"background(#FFFFFF)", //set the background color
+				"smooth()", //make smooth?
+				"}"].join("\n"); //end the function
+				content = []; //the magical content
+ 	for(var i = 0; i < data.length; i++){
+		for(var s = 0; s < data[i].length; s++){
+			
+			content.push(Ax.formats.processing.stroke(data[i][s]))
+			content.push(Ax.formats.processing.strokewidth(data[i][s]))
+			content.push(Ax.formats.processing.fill(data[i][s]))
+			
+			switch(data[i][s].type){
+				case "line":
+				content.push(Ax.formats.processing.line(data[i][s]))
+				break;
+				case "rect":
+				content.push(Ax.formats.processing.rect(data[i][s]))
+				break;
+			}
+		}
+	}
+	return setup+"\nvoid draw(){\n"+content.join("\n")+"\n}";
+ }
+  Ax.formats.processing.stroke = function(shape){
+  	return "stroke("+shape.lineColor+")";
+  }
+  
+  Ax.formats.processing.strokewidth = function(shape){
+  	return "strokeWeight("+shape.lineWidth+")";
+  }
+  
+  Ax.formats.processing.fill = function(shape){
+  	return "fill("+shape.fillColor+")";
+  }
+  
+  Ax.formats.processing.line = function(shape){
+  	return "line("+[shape.left, shape.top, shape.left+shape.width, shape.top+shape.height].join(", ")+")";
+  }
+  
+    Ax.formats.processing.rect = function(shape){
+  	return "rect("+[shape.left, shape.top, shape.width, shape.height].join(", ")+")";
+  }
--- trunk/ajaxanimator/js/drawing/view_wrapper.js
+++ trunk/ajaxanimator/js/drawing/view_wrapper.js
@@ -1,40 +1,39 @@
 /*
-/   wrapper : onlypaths :: view_wrapper : op_view
-*/
-
+ /   wrapper : onlypaths :: view_wrapper : op_view
+ */
 //so basically this is a set of wrapper stuff for op_view, or RichDrawViewer. Its a simplified version of onlypaths that has no dependencies
 //for sole drawing of crap
 
 //the indentation here is screwed cause im using a diffrent ide to make this.
 
 /*
-okay, this file has prettymuch been diversified from a simple preview.js stuff
-to a flexible implementation that probably should work standalone.
-*/
-
+ okay, this file has prettymuch been diversified from a simple preview.js stuff
+ to a flexible implementation that probably should work standalone.
+ */
 //a little hack to get IE to work
 /*
-Meh. This one is from Mozilla, but its too big so i'm gonna steal from elsewhere
-if (!Array.prototype.indexOf){
-  Array.prototype.indexOf = function(elt , from){
-    var len = this.length;
-    var from = Number(arguments[1]) || 0;
-    from = (from < 0)
-         ? Math.ceil(from)
-         : Math.floor(from);
-    if (from < 0) from += len;
-    for (; from < len; from++) {
-      if (from in this && this[from] === elt) return from;
-    }
-    return -1;
-  };
-}
-*/
-
+ Meh. This one is from Mozilla, but its too big so i'm gonna steal from elsewhere
+ if (!Array.prototype.indexOf){
+ Array.prototype.indexOf = function(elt , from){
+ var len = this.length;
+ var from = Number(arguments[1]) || 0;
+ from = (from < 0)
+ ? Math.ceil(from)
+ : Math.floor(from);
+ if (from < 0) from += len;
+ for (; from < len; from++) {
+ if (from in this && this[from] === elt) return from;
+ }
+ return -1;
+ };
+ }
+ */
 //from http://snippets.dzone.com/posts/show/2437
-[].indexOf || (Array.prototype.indexOf = function(v){
-       for(var i = this.length; i-- && this[i] != v;);
-       return i;
+[].indexOf ||
+(Array.prototype.indexOf = function(v){
+    for (var i = this.length; i-- && this[i] != v;) 
+        ;
+    return i;
 });
 
 
@@ -43,41 +42,45 @@ Ax.framerate = 12;
 
 
 Ax.init_view = function(element){
-  element.innerHTML = "";
-  element.style.height = Ax.canvasHeight+"px";
-  element.style.width = Ax.canvasWidth+"px"
-  return Ax.init_view_core(element)
+    element.innerHTML = "";
+    element.style.height = Ax.canvasHeight + "px";
+    element.style.width = Ax.canvasWidth + "px"
+    return Ax.init_view_core(element)
 }
 
 Ax.init_view_core = function(element){
-  if(Ext.isIE == true){
-    var renderer = new VMLRenderer();
-  }else{
-    var renderer = new SVGRenderer();
-  }
-  return new RichDrawViewer(element, renderer);
+    if (Ext.isIE == true) {
+        var renderer = new VMLRenderer();
+    }
+    else {
+        var renderer = new SVGRenderer();
+    }
+    return new RichDrawViewer(element, renderer);
 }
 
 
 Ax.viewer_load_frame = function(frame, layers, canvas, tweenfunc){
-  //note: this function is not multi-layer friendly yet.
-  //un-note: this function should be multi-layer friendly, but layers aren't even really supported so i donno
-  canvas.renderer.removeAll();
-  for(var layer in layers){
-  //alert([1, 2, 3, 4, 5].indexOf(2))
-		if(layers[layer].keyframes.indexOf(frame) != -1){
-			Ax.loadShapes(layers[layer].src[frame],  true, canvas);
-		}else{
-		  Ax.loadShapes(((tweenfunc)?tweenfunc:Ax.getSFTween)(frame, 
-											Ax.largest_nonempty(frame,layer,layers), 
-											Ax.smallest_nonempty(frame,layer,layers),
-											layer,layers[layer].src),  true, canvas);
-		}
-    if(layers[layer].keyframes.sort(function(a,b){return b - a})[0] == frame){
-  		return 0;
-		}
-  }
-  return frame
+    //note: this function is not multi-layer friendly yet.
+    //un-note: this function should be multi-layer friendly, but layers aren't even really supported so i donno
+    canvas.renderer.removeAll();
+    for (var layer in layers) {
+        //alert([1, 2, 3, 4, 5].indexOf(2))
+        if (layers[layer].keyframes.indexOf(frame) != -1) {
+            Ax.loadShapes(layers[layer].src[frame], true, canvas);
+        }
+        else 
+            if (Ax.largest_nonempty(frame, layer, layers) && Ax.smallest_nonempty(frame, layer, layers)) {
+                Ax.loadShapes(((tweenfunc) ? tweenfunc : Ax.getSFTween)(frame, Ax.largest_nonempty(frame, layer, layers), Ax.smallest_nonempty(frame, layer, layers), layer, layers[layer].src), true, canvas);
+            }else{
+				//console.log("Blank Frame")
+			}
+        if (layers[layer].keyframes.sort(function(a, b){
+            return b - a
+        })[0] == frame) {
+            return 0;
+        }
+    }
+    return frame
 }
 
 
