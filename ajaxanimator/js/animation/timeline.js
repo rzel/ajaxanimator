67ff50f784f1383867c1d4685d5efa64547a4ee7
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-23 18:27:41 +0000

    Removed Extraneous Notices Formatted Code

--- ajaxanimator/js/animation/timeline.js
+++ ajaxanimator/js/animation/timeline.js
@@ -1,72 +1,70 @@
-Ax.Timeline = Ext.extend(Ext.Panel,{
-  initComponent: function(){
-
-    Ext.apply(this,{
-      layout:"border",
-      items:[{
-        region:"center",
-        autoScroll: true,
-        border: false,
-        html: "<div id='timeline_core' class='x-unselectable' style='height: 100%; width: 100%'>Loading Frames...</div>"
-      },{
-        region:"west",
-        width: 100,
-        border: false,
-        split:true,
-
-        collapsible:true,
-        collapseMode:"mini",
-        autoScroll: true,
-        layout: "fit",
-        //html: "Layers"
-        items: {
-          id: "layers",
-          xtype:"editorgrid",
-          border:false,
-          hideHeaders: true,
-          plugins: [new Ext.ux.grid.CellActions({
-      callbacks: {
-        "tb_delete":function(grid,record,action,value){
-          Ext.MessageBox.confirm("Delete "+value+" OMG!!!!",
-          "Are you positively super duper sure you want to do this action that your life depends on?!?!?!", function(result){
-		  	if(result == "yes"){
-				Ax.msg("Testing Functionality: Delete "+value,"Attempting to delete the layer using super duper untested tech");
-				
-				Ax.deleteLayer(value)
-			}
-		  })
-          Ax.msg('Callback: delete layer', 'You have clicked: <b>{0}</b>, action: <b>{1}</b>', value, action);
-        }
-      }
-    })],
-          viewConfig:{
-            autoFill: true,
-            forceFit:true
-          },
-          listeners: {
-            "afteredit":function(object){
-              //console.log(object.originalValue,object.value)
-              Ax.renameLayer(object.originalValue,object.value)
-            }
-          },
-          sm: new Ext.grid.RowSelectionModel({singleSelect:true}),
-
-          //clicksToEdit:1,
-          ds:/*BEGIN*/new Ext.data.Store()/*END*/,
-          columns: [
-          {header: "Comment",dataIndex: "comment", editor:new Ext.form.TextField(), cellActions: [
-            {
-              iconCls: "tb_delete",
-              qtip: "Delete this layer."
-            }
-          ]}
-          ]
-        }
-      }]
-    })
-    Ax.Timeline.superclass.initComponent.apply(this, arguments);
-  }
+Ax.Timeline = Ext.extend(Ext.Panel, {
+    initComponent: function(){
+        Ext.apply(this, {
+            layout: "border",
+            items: [{
+                region: "center",
+                autoScroll: true,
+                border: false,
+                html: "<div id='timeline_core' class='x-unselectable' style='height: 100%; width: 100%'>Loading Frames...</div>"
+            }, {
+                region: "west",
+                width: 100,
+                border: false,
+                split: true,
+                
+                collapsible: true,
+                collapseMode: "mini",
+                autoScroll: true,
+                layout: "fit",
+                //html: "Layers"
+                items: {
+                    id: "layers",
+                    xtype: "editorgrid",
+                    border: false,
+                    hideHeaders: true,
+                    plugins: [new Ext.ux.grid.CellActions({
+                        callbacks: {
+                            "tb_delete": function(grid, record, action, value){
+                                Ext.MessageBox.confirm("Delete " + value + "?", "Are you sure you want to delete " + value + "?", function(result){
+                                    if (result == "yes") {
+                                        Ax.deleteLayer(value)
+                                    }
+                                })
+                            }
+                        }
+                    })],
+                    viewConfig: {
+                        autoFill: true,
+                        forceFit: true
+                    },
+                    listeners: {
+                        "afteredit": function(object){
+                            //console.log(object.originalValue,object.value)
+                            Ax.renameLayer(object.originalValue, object.value)
+                        }
+                    },
+                    sm: new Ext.grid.RowSelectionModel({
+                        singleSelect: true
+                    }),
+                    
+                    //clicksToEdit:1,
+                    ds:/*BEGIN*/ new Ext.data.Store()/*END*/,
+                    columns: [{
+                        header: "Comment",
+                        dataIndex: "comment",
+                        editor: new Ext.form.TextField(),
+                        cellActions: [{
+                            iconCls: "tb_delete",
+                            qtip: "Delete this layer."
+                        }]
+                    }]
+                }
+            }]
+        })
+        Ax.Timeline.superclass.initComponent.apply(this, arguments);
+    }
 })
 
-Ext.reg("timeline",Ax.Timeline)
+Ext.reg("timeline", Ax.Timeline)
 
