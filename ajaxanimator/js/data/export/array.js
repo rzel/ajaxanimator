aeb7667f38cf311a1e7da48e082518e9a38eb0e9
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-23 19:54:57 +0000

    backported view_wrapper stuff to array.js

--- ajaxanimator/js/data/export/array.js
+++ ajaxanimator/js/data/export/array.js
@@ -4,28 +4,34 @@
  * It makes making other formats easier
  * Especially those incapable of porting the tweening engine to
  */
-
 Ax.ex.array = function(format){ //returns an array of every single frame (single layered) tweened when necessary
-	Ax.autodiff(); //read the comment in player.js
-    var layers = Ax.export_animation_core(), output = [], frame = 1
+    Ax.autodiff(); //read the comment in player.js
+    var layers = Ax.export_animation_core(), output = [], frame = 1, total_frames = Ax.count_frames(layers);
     for (;; frame++) {
         output.push([]);
         for (var layer in layers) {
             if (layers[layer].keyframes.indexOf(frame) != -1) {
                 output[frame - 1] = output[frame - 1].concat(layers[layer].src[frame])
             }
-            else {
-                output[frame - 1] = output[frame - 1].concat(Ax.getSFTween(frame, Ax.largest_nonempty(frame, layer, layers), Ax.smallest_nonempty(frame, layer, layers), layer, layers[layer].src))
-            }
-            if (layers[layer].keyframes.sort(function(a, b){
-                return b - a;
-            })[0] ==
-            frame) {
-				if (format == "json") {
-					return Ext.util.JSON.encode(output);
-				}else{
-					return output;	
-				}
+            else 
+                if (Ax.largest_nonempty(frame, layer, layers) && Ax.smallest_nonempty(frame, layer, layers)) {
+                    output[frame - 1] = output[frame - 1].concat(Ax.getSFTween(frame, Ax.largest_nonempty(frame, layer, layers), Ax.smallest_nonempty(frame, layer, layers), layer, layers[layer].src))
+                }
+                else 
+                    if (Ax.largest_nonempty(frame, layer, layers)) {
+						output[frame - 1] = output[frame - 1].concat(layers[layer].src[Ax.largest_nonempty(frame, layer, layers)])
+                    }
+                    else {
+                    //console.log("blank frame")
+                    }
+            
+            if (frame >= total_frames) {
+                if (format == "json") {
+                    return Ext.util.JSON.encode(output);
+                }
+                else {
+                    return output;
+                }
             }
         }
     }
@@ -33,6 +39,6 @@ Ax.ex.array = function(format){ //returns an array of every single frame (single
 }
 
 Ax.ex.array.save = function(){
-	Ax.msg("Notes on exporting to OnlyPaths JSON Frame List","Export to OnlyPaths JSON Frame List does not contain layer or tween data.")
-	Ax.save.computer(Ext.util.JSON.encode(Ax.ex.array()),Ax.animation.name+".opf");
+    Ax.msg("Notes on exporting to OnlyPaths JSON Frame List", "Export to OnlyPaths JSON Frame List does not contain layer or tween data.")
+    Ax.save.computer(Ext.util.JSON.encode(Ax.ex.array()), Ax.animation.name + ".opf");
 }
