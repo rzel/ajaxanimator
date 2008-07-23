5986cb0588068d8db55fde79388b1ae2c56eb2d7
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-23 18:28:39 +0000

    Formatted Code

--- trunk/ajaxanimator/js/animation/controls.js
+++ trunk/ajaxanimator/js/animation/controls.js
@@ -1,45 +1,45 @@
 Ax.controls = {
-  play: function(){
-    Ax.viewport.findById("maintabpanel").activate(1)
-    Ax.preview_increment();
-  },
-  pause: function(){
-    Ax.viewport.findById("maintabpanel").activate(1)
-	clearTimeout(Ax.preview_timeout);
-    clearTimeout(Ax.preview_timeout);
-    clearTimeout(Ax.preview_timeout);
-  },
-  next: function(){
-	  	switch(Ax.viewport.findById("maintabpanel").getActiveTab().getId()){
-		case "canvas_tab":
-		Ax.selectFrame(Ax.tcurrent.frame + 1, Ax.tcurrent.layer)
-		break;
-		case "preview_tab":
-		Ax.preview_frame++;
-		break;
-	}
-  },
-  previous: function(){
-	  	switch(Ax.viewport.findById("maintabpanel").getActiveTab().getId()){
-		case "canvas_tab":
-		Ax.selectFrame(Ax.tcurrent.frame-1,Ax.tcurrent.layer)
-		break;
-		case "preview_tab":
-		Ax.preview_frame--;
-		break;
-	}
-  },
-  last: function(){
-    Ax.msg("No worky","this doesn't work yet.")
-  },
-  first: function(){
-  	switch(Ax.viewport.findById("maintabpanel").getActiveTab().getId()){
-		case "canvas_tab":
-		Ax.selectFrame(1, Ax.tcurrent.layer);
-		break;
-		case "preview_tab":
-		Ax.preview_frame = 1;
-		break;
-	}
-  }
+    play: function(){
+        Ax.viewport.findById("maintabpanel").activate(1)
+        Ax.preview_increment();
+    },
+    pause: function(){
+        Ax.viewport.findById("maintabpanel").activate(1)
+        clearTimeout(Ax.preview_timeout);
+        clearTimeout(Ax.preview_timeout);
+        clearTimeout(Ax.preview_timeout);
+    },
+    next: function(){
+        switch (Ax.viewport.findById("maintabpanel").getActiveTab().getId()) {
+            case "canvas_tab":
+                Ax.selectFrame(Ax.tcurrent.frame + 1, Ax.tcurrent.layer)
+                break;
+            case "preview_tab":
+                Ax.preview_frame++;
+                break;
+        }
+    },
+    previous: function(){
+        switch (Ax.viewport.findById("maintabpanel").getActiveTab().getId()) {
+            case "canvas_tab":
+                Ax.selectFrame(Ax.tcurrent.frame - 1, Ax.tcurrent.layer)
+                break;
+            case "preview_tab":
+                Ax.preview_frame--;
+                break;
+        }
+    },
+    last: function(){
+        Ax.msg("No worky", "this doesn't work yet.")
+    },
+    first: function(){
+        switch (Ax.viewport.findById("maintabpanel").getActiveTab().getId()) {
+            case "canvas_tab":
+                Ax.selectFrame(1, Ax.tcurrent.layer);
+                break;
+            case "preview_tab":
+                Ax.preview_frame = 1;
+                break;
+        }
+    }
 }
