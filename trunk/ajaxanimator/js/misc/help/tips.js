cd319be9005cd9d53baf21b763d9bfd5a479d013
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-28 20:45:38 +0000

    Working Tips

--- trunk/ajaxanimator/js/misc/help/tips.js
+++ trunk/ajaxanimator/js/misc/help/tips.js
@@ -8,47 +8,67 @@
  * I'm debating whether I should make this appear when you start it. woudl that work?
  * I'm probably talking to my self right now. and hopefully nobody will read this. but you nver know.
  */
-
-
 //.... yes i did..... i totally copied Kate :P
 
-Ax.tips_array = ["Tips Not Loaded"];
-
+Ax.tips_array = [];
 
 Ax.showTips = function(){
-if(!Ax.tipsWindow){
-  Ax.tipsWindow = new Ext.Window({
-    title: "Tip of the Day - Ajax Animator",
-    iconCls: "tb_tip",
-    layout: "border",
-    width: 300,
-    height: 250,
-    items: [
-            {region: "north", html: "<center><h1 class='tiptitle'>Did you know...?</h1></center><br>", border: false},
-            {region: "center", html: "<center>Loading...</center>", border: false}
-            ],
-    buttons: [{text: "Previous", iconCls: "arrow_prev"},
-    {text: "Next", iconCls: "arrow_next", handler: function(){
-    this.ownerCt.layout.center.panel.body.update(Ax.getTipData())
-    }},
-    {text: "Close", iconCls: "close", handler: function(){this.ownerCt.close()}}]
-    })
+    if (!Ax.tipsWindow) {
+        Ax.tipsWindow = new Ext.Window({
+            title: "Tip of the Day - Ajax Animator",
+            iconCls: "tb_tip",
+            layout: "border",
+            width: 300,
+            height: 250,
+            items: [{
+                region: "north",
+                html: "<center><h1 class='tiptitle'>Did you know...?</h1></center><br>",
+                border: false
+            }, {
+                region: "center",
+                html: Ax.getTipData(),
+                border: false
+            }],
+            buttons: [{
+                text: "Previous",
+                iconCls: "arrow_prev",
+				handler: function(){
+					Ax.tipIndex = (Ax.tipIndex-1) % Ax.tips_array.length;
+					this.ownerCt.layout.center.panel.body.update(Ax.getTipData())
+				}
+            }, {
+                text: "Next",
+                iconCls: "arrow_next",
+                handler: function(){
+					Ax.tipIndex = (Ax.tipIndex+1) % Ax.tips_array.length
+                    this.ownerCt.layout.center.panel.body.update(Ax.getTipData())
+                }
+            }, {
+                text: "Close",
+                iconCls: "close",
+                handler: function(){
+                    this.ownerCt.close()
+                }
+            }]
+        })
     }
     Ax.tipsWindow.show(document.body)
 }
 
 
 Ax.getTipData = function(){
-if(!Ax.tipsWindow){
-Ax.showTips();
-}
-if(!Ax.tipIndex){
-  Ax.tipIndex = Math.floor(Ax.tips_array.length*Math.random())
-}
-return "<center>"+Ax.tips_array[Ax.tipIndex]+"</center>";
-}
-
-Ax.tipsNext = function(){
-
-return Ax.getTipData();
+    
+    if (!Ax.tipIndex) {
+	
+	//var today = 
+	
+	Ax.tipIndex = Math.floor( //floor it
+	((new Date()).getTime() //get today's date
+	- 2*2*2*2*2*3*3*42227*100000//Yes, I have an iPhone
+	)/( //divide it by
+	1000*60*60*24//A Day
+	) % Ax.tips_array.length); //and get the remainder
+	
+    }
+    return "<center>" + Ax.tips_array[Ax.tipIndex] + "</center>";
 }
