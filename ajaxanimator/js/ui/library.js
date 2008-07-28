56e6677c6aa6c2b5ebe057e25b6587a3234539ba
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-07-28 21:58:46 +0000

    Working Library. Added Tree to library koolionessity

--- ajaxanimator/js/ui/library.js
+++ ajaxanimator/js/ui/library.js
@@ -1,26 +1,46 @@
 /*
-Tree View for Library
-*/
-
-Ax.Library = Ext.extend(Ext.tree.TreePanel,{
-  initComponent: function(){
-  Ext.apply(this,{
-    xtype:"treepanel",
-    animate:true,
-    autoScroll:true,
-    containerScroll:true,
-    root:new Ext.tree.TreeNode({text:'Tree Root',draggable : false}),
-    dropConfig:{
-      appendOnly:true
-    },
-    border:false
-
-  });
-    Ax.Library.superclass.initComponent.apply(this, arguments);
-  }
-
+ Tree View for Library
+ */
+Ax.Library = Ext.extend(Ext.tree.TreePanel, {
+    initComponent: function(){
+        Ext.apply(this, {
+            xtype: "treepanel",
+            useArrows: true,
+            autoScroll: true,
+            animate: true,
+            enableDD: false,
+            containerScroll: true,
+            root: new Ext.tree.AsyncTreeNode({
+                text: 'Shared',
+                expanded: true,
+                draggable: false,
+                id: '.'
+            }),
+            loader: new Ext.tree.TreeLoader({
+                dataUrl: Ax.files.library
+            }),
+            dropConfig: {
+                appendOnly: true
+            },
+            border: false
+        
+        });
+		this.on("click", function(node){
+                    if (node.childrenRendered == false) {
+                        Ext.Ajax.request({
+                            url: Ax.files.libraryitem + node.id,
+                            success: function(e){
+								Ax.loadShapes(e.responseText)
+                            }
+                        })
+                    }
+                })
+        Ax.Library.superclass.initComponent.apply(this, arguments);
+    }
+    
 });
 
-Ext.reg("library",Ax.Library);
+Ext.reg("library", Ax.Library);
 
-
+                
+                
