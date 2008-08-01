baab17c539df3b748aa3c2d96ec3fa558408d960
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-08-01 23:53:01 +0000

    pwetty properties

--- ajaxanimator/js/ui/panels/south.js
+++ ajaxanimator/js/ui/panels/south.js
@@ -1,11 +1,10 @@
 Ax.LayoutSouthPanel = Ext.extend(Ext.Panel,{
 initComponent: function(){
 Ext.apply(this,{
-	collapsedTitle: "Properties",
     region:"south",
     id: "properties",
 	iconCls: "app_settings",
-    title:"Properties",
+    title:"Properties - Animation",
     split:true,
     collapsible:true,
     titleCollapse:true,
@@ -14,32 +13,7 @@ Ext.apply(this,{
 	xtype: "form",
 	labelWidth: 50,
 	border: false,
-	items: [{
-	layout: "column",
-	defaults: {
-	 width: 120
-	 },
-	border: false,
-	items: [{
-	layout: "form",
-	border: false,
-	items: [{
-		xtype: "numberfield",
-		fieldLabel: "Width",
-		name: "Width",
-		width: 60
-	}]
-	},{
-	layout: "form",
-	border: false,
-	items: [{
-		xtype: "numberfield",
-		fieldLabel: "Height",
-		name: "Width",
-		width: 60
-	}]
-	}]
-	}]
+	items: {xtype: "properties"}
 	}]
   })
    this.initialConfig.collapsible = true; //bugfix from http://outroot.com/extjs/bug1/ 
--- ajaxanimator/js/ui/properties.js
+++ ajaxanimator/js/ui/properties.js
@@ -1,21 +1,96 @@
 //Various forms defining properties
-
+Ax.PropertiesPanel = Ext.extend(Ext.Panel,{
+initComponent: function(){
+Ext.apply(this,{
+border: false,
+id: "properties_form",
+layout: "card",
+activeItem: 0,
 items: [{
-	xtype: "form",
-	labelWidth: 50,
+	layout: "column",
+	defaults: {
+	 width: 120
+	 },
 	border: false,
 	items: [{
-		layout: "column",
-		border: false,
-		items: [{
-			layout: "form",
-			border: false,
-			items: [{
-				xtype: "numberfield",
-				fieldLabel: "Width",
-				width: 60,
-				name: "Width"
-			}]
-		}]
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Width",
+		name: "width",
+		width: 60
+	}]},{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Height",
+		name: "height",
+		width: 60
 	}]
-}]
+	},{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Framerate",
+		name: "framerate",
+		width: 60
+	}]
+	}]
+	},{
+	layout: "column",
+	defaults: {
+	 width: 120
+	 },
+	border: false,
+	items: [{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Width",
+		name: "Width",
+		width: 60
+	}]},{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Height",
+		name: "Width",
+		width: 60
+	}]
+	},{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "X (Left)",
+		name: "X",
+		width: 60
+	}]
+	},{
+	layout: "form",
+	border: false,
+	items: [{
+		xtype: "numberfield",
+		fieldLabel: "Y (Top)",
+		name: "Y",
+		width: 60
+	}]
+	}]
+	}]
+	
+	  })
+   Ax.PropertiesPanel.superclass.initComponent.apply(this, arguments);
+  }
+  })
+  
+  Ext.reg("properties",Ax.PropertiesPanel);
+  
+  Ax.setPropertiesMode = function(mode){
+  Ax.viewport.findById("properties").setTitle(Ax.viewport.findById("properties").title.split("-")[0]+"- "+mode);
+  }
+  
