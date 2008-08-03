5be744b48fe5254fde97799ed542f91b781140f8
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-08-03 23:12:26 +0000

    Kommited some Kompression stuffs

---
+++ ajaxanimator/build/compress.bat
@@ -0,0 +1,3 @@
+java -jar ../server/dev/compile/yuicompressor.jar --type js -v -o ajaxanimator-cmp.js ajaxanimator-all.js
+java -jar ../server/dev/compile/yuicompressor.jar --type css -v -o ajaxanimator-cmp.css ajaxanimator-all.css
+pause
