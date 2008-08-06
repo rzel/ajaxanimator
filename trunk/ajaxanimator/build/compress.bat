721611eb46f0e8998151d07549e47a362f2799b7
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-08-06 16:00:14 +0000

    Some tiny stuffs

--- trunk/ajaxanimator/build/compress.bat
+++ trunk/ajaxanimator/build/compress.bat
@@ -1,3 +1,5 @@
+copy index.htm deployindex.htm
+pause
 java -jar ../server/dev/compile/yuicompressor.jar --type js -v -o ajaxanimator-cmp.js ajaxanimator-all.js
 java -jar ../server/dev/compile/yuicompressor.jar --type css -v -o ajaxanimator-cmp.css ajaxanimator-all.css
 pause
