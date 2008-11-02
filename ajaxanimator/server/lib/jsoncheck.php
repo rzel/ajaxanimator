926facabbaccee9d5a23a50075ff6d3da9bfcf70
antimatter15 <antimatter15@2afa3a53-7631-0410-98bc-455b2d0be069>
2008-11-02 00:58:34 +0000

    better indents

--- ajaxanimator/server/lib/jsoncheck.php
+++ ajaxanimator/server/lib/jsoncheck.php
@@ -1,29 +1,26 @@
 <?php
 /*
 Copyright Antimatter15 2008
-
+(Under GPL like the rest of the Ajax Animator)
 Checks whether the server has json_encode, and if not, includes
  the Pear JSON package, and creates a json_encode function. 
 */
 
 
 if(!function_exists("json_encode")){
-include "JSON.php"; //use requre_once if needed, but include is faster
+  include "JSON.php"; //use requre_once if needed, but include is faster
 
-function json_encode($data){
-	$value = new Services_JSON();
-	return $value->encode($data); 
+  function json_encode($data){
+    $value = new Services_JSON();
+    return $value->encode($data); 
+  }
+  function json_decode($data,$assoc){
+    if($assoc == true){
+    $value = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+    }else{
+    $value = new Services_JSON();
+    }
+    return $value->decode($data); 
+  }
 }
-function json_decode($data,$assoc){
-	if($assoc == true){
-	$value = new Services_JSON(SERVICES_JSON_LOOSE_TYPE);
-	}else{
-	$value = new Services_JSON();
-	}
-	return $value->decode($data); 
-}
-}
-
-
-
 ?>
